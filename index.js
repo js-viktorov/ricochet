424341ef13392bfcee43f4930535aaa905d6f308
roshek <roshek34@mail.ru>
2018-06-14 19:58:01 +0000

    path handler adjustments

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var lib = require('./lib/entry.js');
 
 lib.wsserver({
-	"http://149.154.167.51:80/api":lib.wsserver.collectHandler((data,res,req,rqSender)=>{
+	"/api":lib.wsserver.collectHandler((data,res,req,rqSender)=>{
 		res.writeHeaders = headers=>res.writeHead(200,headers);
 		res.error = code=>{
 			res.statusCode = code;
 			res.end();
 		};
 		rqSender(data).into(res).end();
-	},lib.fixedproxy.integral(lib.requestOptions.tgDirectOptions)),
+	},lib.fixedproxy.integral(lib.requestOptions.toProxy)),
 },{},process.env.PORT || 8080).then(port=>log(`App up on http://localhost:${port}`));
