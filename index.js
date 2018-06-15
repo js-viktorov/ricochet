13bf4135948808a7cf47adf4572b6f82eb1c3399
roshek <roshek34@mail.ru>
2018-06-15 20:25:26 +0000

    ws channel impl, fail-safe measures

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 var lib = require('./lib/entry.js');
 
+var wsRqSender = lib.fixedproxy.integral(lib.requestOptions.toTG,true);
+
 lib.wsserver({
 	"/api":lib.wsserver.collectHandler((data,res,req,rqSender)=>{
-		console.log('Processing request');
 		res.writeHeaders = headers=>res.writeHead(200,headers);
 		res.error = code=>{
-			res.statusCode = code;
-			res.end();
+			if(!res.socket.destroyed){
+				res.statusCode = code;
+				res.end();
+			}
 		};
 		rqSender(data).into(res).end();
 	},lib.fixedproxy.integral(lib.requestOptions.toTG)),
-},{},process.env.PORT || 8080).then(port=>log(`App up on http://localhost:${port}`));
+},{
+	"/wsapi":ws=>{
+		console.log('ws connected');
+		ws.on('message',lib.oneAtATime(m=>{
+			wsRqSender(m).into({
+				error:code=>{
+					console.log('error in wsapi');
+					ws.send([]);
+				},
+				...lib.wsserver.collectStream(data=>ws.send(data))
+			},true)
+		}));
+	}
+},process.env.PORT || 8080).then(port=>console.log(`App up on http://localhost:${port}`));
+
+process.on('uncaughtException', function (err) {
+	console.log('uncaught', err);
+});
+process.on('uncaughtRejection', function (err) {
+	console.log('uncaughtRejection', err);
+});
