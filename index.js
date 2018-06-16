3c648ee9a75fc2efe4024d97c99f2081168f8b24
roshek <roshek34@mail.ru>
2018-06-16 12:05:56 +0000

    logs adjusted, ws counter

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 var lib = require('./lib/entry.js');
 
-var wsRqSender = lib.fixedproxy.integral(lib.requestOptions.toTG,true);
+var rqSender = lib.fixedproxy.integral(lib.requestOptions.toTG);
+
+var wsConnectionsCount = 0;
 
 lib.wsserver({
-	"/api":lib.wsserver.collectHandler((data,res,req,rqSender)=>{
+	"/api":lib.wsserver.collectHandler((data,res,req)=>{
 		res.writeHeaders = headers=>res.writeHead(200,headers);
 		res.error = code=>{
 			if(!res.socket.destroyed){
+				console.log('remote error processing http ' + code);
 				res.statusCode = code;
 				res.end();
 			}
 		};
 		rqSender(data).into(res).end();
-	},lib.fixedproxy.integral(lib.requestOptions.toTG)),
+	}),
 },{
 	"/wsapi":ws=>{
-		console.log('ws connected');
+		console.log('ws connected, total ' + (++wsConnectionsCount));
 		ws.on('message',lib.oneAtATime(m=>{
-			wsRqSender(m).into({
+			rqSender(m).into({
+				writeHeaders:$=>{},
 				error:code=>{
-					console.log('error in wsapi');
+					console.log('remote error processing ws ' + code);
 					ws.send([]);
 				},
 				...lib.wsserver.collectStream(data=>ws.send(data))
 			},true)
 		}));
+		ws.on('close',$=>console.log('ws disconnected, total ' + (--wsConnectionsCount)));
 	}
 },process.env.PORT || 8080).then(port=>console.log(`App up on http://localhost:${port}`));
 
